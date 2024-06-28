f77b0f5c4abf837b12a2252c38aafae1cf6e3d6f
Loges <sivalog25@gmail.com>
2024-06-28 13:09:54 +0530

    api testing loges

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('routes');
+const routes = require('./router');
 
 const app = express();
 
 app.use(express.json())
+app.use('/api',routes)
 
+//connection to mongoose
+mongoose.connect('mongodb+srv://sivalog25:system@mernauth.3victko.mongodb.net/?retryWrites=true&w=majority&appName=MernAuth',{
+   useNewUrlParser:true,
+   useUnifiedTopology:true, 
+})
+
+const database = mongoose.connection
+
+database.on('error',(err)=>console.log(err))
+
+database.on("connected",()=>console.log('Database connected'))
 app.listen(3000, ()=>{
     console.log("server started on localhost:3000");
 })
